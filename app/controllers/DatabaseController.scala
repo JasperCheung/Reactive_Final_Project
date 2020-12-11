da2ffc08cbd903dbcaba39c294b62619a9eae005
Hannan Abid <abidh0601@gmail.com>
2020-12-10 23:22:38 -0500

    Added backend updateEntry and createEntry

--- app/controllers/DatabaseController.scala
+++ app/controllers/DatabaseController.scala
@@ -10,7 +10,8 @@ import play.db.NamedDatabase
 import scala.concurrent.Future
 
 import scala.concurrent.ExecutionContext
-
+import java.time.LocalDateTime
+import akka.http.javadsl.model.DateTime
 
 
 
@@ -100,11 +101,80 @@ class DatabaseController @Inject()(@NamedDatabase("db") diaryDatabase: Database,
           case _: Throwable =>
         }
       }
-
+      
+      
       Ok(Json.obj("success" -> true, "uid" -> uid))
     }.getOrElse(Ok(Json.obj("success" -> false, "uid" -> -1)))
 
   }
 
+  def createEntry = Action { request =>
+    val postVals = request.body.asFormUrlEncoded
+    var success = false
+
+    postVals.map { args =>
+      ///need to replace args("uid") with cookies
+      val uid = args("uid").head
+      val title = args("title").head
+      val content = args("content").head
+      val dateTime = DateTime.now()
+
+      
+
+      diaryDatabase.withConnection{ conn =>
+        val statement = conn.createStatement()
+        try {
+          val diaryCreated= statement.executeUpdate(s"INSERT INTO Entries (User_id, Title, Content, Time_created) VALUES ('$uid', '$title', '$content', '$dateTime')")
+          if(diaryCreated != 0)
+          {
+            success = true
+          }
+          
+        }
+        catch {
+          case _: Throwable =>
+        }
+      }
+
+      Ok(Json.obj("success" -> success))
+
+    }.getOrElse(Ok(Json.obj("success" -> success)))
+
+
+  }
+
+  def updateEntry = Action {request =>
+    var success = false
+
+    val postVals = request.body.asFormUrlEncoded
+
+    postVals.map { args =>
+      val id = args("id").head
+      val content = args("content").head
+      val dateTime = DateTime.now()
+
+      diaryDatabase.withConnection{ conn =>
+      val statement = conn.createStatement()
+
+      try { 
+        val diaryUpdated = statement.executeUpdate(s"UPDATE Entries SET Content = '$content', Time_created = '$dateTime' WHERE  id = '$id'")
+
+        if(diaryUpdated != 0){
+        
+          success = true
+        }
+
+      }
+      catch{
+        case _: Throwable => 
+      }
+
+      
+    }
+
+    Ok(Json.obj("success" -> success))
+    }.getOrElse( Ok(Json.obj("success" -> success)))
+
+  }
 
 }
